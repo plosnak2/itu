1a9958ccac81e6ace8caa62726be227cc5d015ae
Jozcas <jozefcasar512@gmail.com>
2021-11-21 18:35:22 +0100

    saving working filter before making changes

--- itu-proj/Components/Home.js
+++ itu-proj/Components/Home.js
@@ -3,21 +3,39 @@ import { ScrollView, Image, Text, View, TouchableOpacity, StyleSheet, StatusBar,
 import HomeScreen from '../Screens/homepage';
 import { RecipeRef } from '../firebaseConfig';
 import Navbar from '../Static/Navbar';
+import Dropdown from './Dropdown';
 
 class Home extends Component {
     state = {
         Recipe: [],
         subscribe: 'a',
+        filter: '',
+    }
+
+    async get_data(filter){
+        RecipeRef.onSnapshot((QuerySnapshot) => {
+            let recipes = [];
+            this.setState({Recipe: []})
+            QuerySnapshot.forEach((doc) => {
+                if(filter != ''){
+                    let reci = doc.data()
+                    console.log("Som tu ")
+                    
+                          filter.forEach((ingredient) => {if(ingredient in reci.ingredient){recipes.push({ id: doc.id, data: doc.data() });}})
+                    ;
+                }
+                else if(filter == ''){
+                    console.log("tu nechcem byt")
+                recipes.push({ id: doc.id, data: doc.data() });
+                }
+            });
+            console.log(recipes)
+            this.setState({ Recipe: recipes });
+        });
     }
 
     componentDidMount() {
-        RecipeRef.onSnapshot((QuerySnapshot) => {
-            let recipes = [];
-            QuerySnapshot.forEach((doc) => {
-                recipes.push({ id: doc.id, data: doc.data() });
-            });
-            this.setState({ Recipe: recipes });
-        });
+        this.get_data('');
             /*let imageRef = firebase.storage().ref('/AjeuQGuaecKrhM4pUgb9.png');
             imageRef
             .getDownloadURL()
@@ -32,10 +50,31 @@ class Home extends Component {
         }
         return (rate/rate_count)
     }
+
+    filtered_ingredients = (filter) => {
+        console.log(filter)
+        this.get_data(filter);
+        console.log(this.state.Recipe)
+        this.render();
+        this.setState({filter: filter})
+          /*let recipe_ingredient = [];
+          if(filter != ''){
+          this.state.Recipe.map((item, index) => (
+                filter.forEach((ingredient) => {if(ingredient in item.data.ingredient){console.log(this.state.filter);console.log(item.data.name); this.componentDidMount()}})
+          ));
+          }*/
+          //this.componentDidMount
+    }
+
     render() {
         return (
             <View style={{flex:1}}>
+                <View style={{marginTop: Platform.OS === 'android' ? StatusBar.currentHeight : Platform.OS === 'ios' ? 40 : 0,}}>
+                    <Dropdown set={this.filtered_ingredients}/>
+                </View>
+                
             <ScrollView style={styles.content}>
+                
                 {
                     this.state.Recipe.map((item, index) => (
                         <TouchableOpacity
